c12ba3b7fbccb7a355b862742d87b4fe9a8a8fa8
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-04-09 17:31:56 +0530

    CRUD Setup

---
+++ CyberCrimeComplaintPortal/Controllers/ComplaintController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CyberCrimeComplaintPortal.Data;
+using CyberCrimeComplaintPortal.Models;
+
+namespace CyberCrimeComplaintPortal.Controllers
+{
+    public class ComplaintController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ComplaintController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var complaints = _db.Complaints.ToList();
+            return View(complaints);
+        }
+
+
+        // GET: Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Complaint obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Complaints.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Complaint Submitted Successfully!";
+                return RedirectToAction(nameof(Index)); // can change this later
+            }
+            return View(obj);
+        }
+
+
+    }
+}
---
+++ CyberCrimeComplaintPortal/Migrations/20250409050312_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CyberCrimeComplaintPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Complaints",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IncidentDetails = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateSubmitted = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Complaints", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Complaints");
+        }
+    }
+}
