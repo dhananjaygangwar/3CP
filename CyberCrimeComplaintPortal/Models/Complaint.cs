bf018f64a5beb83ff30373497a64a44b743d867b
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-04-09 09:59:39 +0530

    Testing

--- CyberCrimeComplaintPortal/Models/Complaint.cs
+++ CyberCrimeComplaintPortal/Models/Complaint.cs
@@ -1,7 +1,28 @@
-﻿namespace CyberCrimeComplaintPortal.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CyberCrimeComplaintPortal.Models
 {
     public class Complaint
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string IncidentDetails { get; set; }
+
+        public string status { get; set; } = "Pending"; //default
+
+        public DateTime DateSubmitted { get; set; }
+
     }
 }
