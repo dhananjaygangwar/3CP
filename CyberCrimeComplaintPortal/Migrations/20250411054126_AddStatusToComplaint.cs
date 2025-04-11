abea8e29f60e3b7e19e665f8f63c511c1716ff6e
Dhananjay Gangwar <kannu.2097@gmail.com>
2025-04-11 11:25:08 +0530

    Cool

---
+++ CyberCrimeComplaintPortal/Migrations/20250411054126_AddStatusToComplaint.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CyberCrimeComplaintPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusToComplaint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "status",
+                table: "Complaints",
+                newName: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Complaints",
+                newName: "status");
+        }
+    }
+}
